fa8344fa727244d079450c852f5c5f7ac00c8d3d
242darkman <bvouvou@gmail.com>
2024-04-11 21:14:09 +0200

    feat: add firebase configuration

---
+++ global-renov-api/lib/src/config/firebase_config.dart
@@ -0,0 +1,9 @@
+final Map<String, dynamic> firebaseConfig = {
+  "apiKey": "AIzaSyDP0lsaX57M0-EicHfm4VYRzqDVDl3An9U",
+  "authDomain": "global-renov.firebaseapp.com",
+  "projectId": "global-renov",
+  "storageBucket": "global-renov.appspot.com",
+  "messagingSenderId": "967932740530",
+  "appId": "1:967932740530:web:83270cab7b081361455aab",
+  "measurementId": "G-DZCBJD0L9V"
+};
