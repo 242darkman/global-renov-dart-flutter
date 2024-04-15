32486abe2fc0bb125f73183cc9ed658891f3c3f9
242darkman <bvouvou@gmail.com>
2024-04-15 18:37:56 +0200

    fix: modification of the file containing environment variables for firebase configuration

---
+++ global-renov-api/lib/src/config/firebase_data_config.dart
@@ -0,0 +1,13 @@
+class FirebaseDataConfig {
+  final Map<String, dynamic> dataConfiguration = {
+    "apiKey": "AIzaSyDP0lsaX57M0-EicHfm4VYRzqDVDl3An9U",
+    "authDomain": "global-renov.firebaseapp.com",
+    "projectId": "global-renov",
+    "storageBucket": "global-renov.appspot.com",
+    "messagingSenderId": "967932740530",
+    "appId": "1:967932740530:web:83270cab7b081361455aab",
+    "measurementId": "G-DZCBJD0L9V",
+    "databaseURL":
+        "https://global-renov-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+}
