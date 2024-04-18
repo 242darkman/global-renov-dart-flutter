7691d497851eeaddc9f23c1392f80395abb05d9d
242darkman <bvouvou@gmail.com>
2024-04-18 03:05:06 +0200

    fix: splitting the login page code into one screen

--- global-renov-client/lib/main.dart
+++ global-renov-client/lib/main.dart
@@ -1,112 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:global_renov/screens/home.dart';
+import 'package:global_renov/screens/auth/login_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferenceService.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Centrer le contenu
-              children: [
-                Image.asset(
-                  'assets/logo.png',
-                  width: 250,
-                  height: 200,
-                ),
-                _buildFormContainer(),
-              ],
-            ),
-          ),
-        ),
+    return MultiProvider(
+      providers: [
+      ],
+      child: MaterialApp(
+        title: 'Global\'Renov',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(),
+        home: LoginScreen(),
       ),
     );
   }
-
-  Widget _buildFormContainer() {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-        color: _formContainerColor.withOpacity(0.9),
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(6.0),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Email',
-              prefixIcon: Icon(Icons.email),
-            ),
-          ),
-          const SizedBox(height: 30),
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Mot de passe',
-              prefixIcon: Icon(Icons.lock),
-            ),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeApp()),
-              );
-            },
-            child: const Text(
-              'Se connecter',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: _buttonColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-          ),
-          const SizedBox(height: 30),
-        ],
-      ),
-    );
-  }
-
-  static const Color _formContainerColor = Colors.white;
-  static const Color _buttonColor = Color(0xFF55895B);
 }
---
+++ global-renov-client/lib/screens/auth/login_screen.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:global_renov/screens/auth/register_screen.dart';
+import 'package:global_renov/screens/intervention/intervention_list_screen.dart';
+import 'package:global_renov/services/auth_service.dart';
+import 'package:global_renov/state/app_state.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  static const Color _formContainerColor = Colors.white;
+  static const Color _buttonColor = Color(0xFF55895B);
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+
+    return PlatformScaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/logo.png', width: 250, height: 200),
+                _buildFormContainer(authService),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFormContainer(AuthService authService) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: _formContainerColor.withOpacity(0.9),
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(6.0),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildEmailField(),
+          const SizedBox(height: 20),
+          _buildPasswordField(),
+          const SizedBox(height: 20),
+          _buildLoginButton(authService),
+          const SizedBox(height: 10),
+          _buildCreateAccountButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmailField() {
+    return PlatformTextField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      material: (_, __) => MaterialTextFieldData(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(color: _buttonColor),
+          ),
+          labelText: 'Email',
+          labelStyle: TextStyle(color: _buttonColor),
+          prefixIcon: Icon(Icons.email),
+        ),
+      ),
+      cupertino: (_, __) => CupertinoTextFieldData(
+        placeholder: 'Email',
+        prefix: const Icon(Icons.email, color: Colors.grey),
+        placeholderStyle: const TextStyle(color: _buttonColor),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return PlatformTextField(
+      controller: _passwordController,
+      obscureText: true,
+      material: (_, __) => MaterialTextFieldData(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(color: _buttonColor),
+          ),
+          labelText: 'Mot de passe',
+          labelStyle: TextStyle(color: _buttonColor),
+          prefixIcon: Icon(Icons.lock),
+        ),
+      ),
+      cupertino: (_, __) => CupertinoTextFieldData(
+          placeholder: 'Mot de passe',
+          prefix: const Icon(Icons.lock, color: Colors.grey),
+          placeholderStyle: const TextStyle(color: _buttonColor)),
+    );
+  }
+
+  Widget _buildLoginButton(AuthService authService) {
+    return PlatformElevatedButton(
+      child: const Text(
+        'Se connecter',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () => _handleLogin(authService),
+      material: (_, __) => MaterialElevatedButtonData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: _buttonColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+      cupertino: (_, __) => CupertinoElevatedButtonData(
+        color: _buttonColor,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    );
+  }
+
+  Widget _buildCreateAccountButton() {
+    return PlatformTextButton(
+      onPressed: _handleCreateAccount,
+      material: (_, __) => MaterialTextButtonData(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(
+            color: Colors.grey,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      ),
+      cupertino: (_, __) => CupertinoTextButtonData(
+        child: const DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.grey,
+            decoration: TextDecoration.underline,
+          ),
+          child: Text('Pas de compte ? Créez-en un'),
+        ),
+      ),
+      child: const Text('Pas de compte ? Créez-en un',
+          style: TextStyle(color: Colors.grey)),
+    );
+  }
+
+  void _handleLogin(AuthService authService) async {
+    try {
+      var result = await authService.signIn(
+          _emailController.text, _passwordController.text);
+      if (result != null) {
+        Provider.of<AppState>(context, listen: false).setUser(result);
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const InterventionList()));
+      } else {
+        _showErrorDialog(
+            'Informations d\'identification non valides. Veuillez réessayer.');
+      }
+    } catch (e) {
+      _showErrorDialog('Erreur de connexion: ${e.toString()}');
+    }
+  }
+
+  void _handleCreateAccount() {
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => const RegisterScreen()));
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Erreur'),
+        content: Text(message),
+        actions: [
+          PlatformDialogAction(
+            child: PlatformText('D\'accord'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
