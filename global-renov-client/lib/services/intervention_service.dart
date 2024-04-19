ee21f6ba282e507da3365b92364232e5fbde9c37
242darkman <bvouvou@gmail.com>
2024-04-19 03:25:44 +0200

    feat: add `GET` method which allows us to retrieve a single intervention

--- global-renov-client/lib/services/intervention_service.dart
+++ global-renov-client/lib/services/intervention_service.dart
@@ -11,6 +11,41 @@ class InterventionService {
     authToken = PreferenceService.getToken();
   }
 
+
+  /// Asynchronously retrieves a single intervention by [idIntervention].
+  ///
+  /// Sends a GET request to the API endpoint `${Environment.apiUrl}/interventions/$idIntervention`
+  /// with the necessary headers, including the authorization token obtained from `PreferenceService.getToken()`.
+  ///
+  /// If the request is successful (status code 200), the function returns the decoded response body as a `Map<String, dynamic>`.
+  /// If an error occurs during the request, the function prints the error and returns `null`.
+  ///
+  /// Parameters:
+  /// - `idIntervention`: The ID of the intervention to retrieve.
+  ///
+  /// Returns a `Future<Map<String, dynamic>?>` representing the result of the API call.
+  Future<Map<String, dynamic>?> fetchSingleIntervention(
+      String idIntervention) async {
+    try {
+      String? authToken = PreferenceService.getToken();
+      final response = await http.get(
+        Uri.parse('${Environment.apiUrl}/interventions/$idIntervention'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $authToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+    } catch (e) {
+      log.severe('Error retrieving interventions : $e');
+      return null;
+    }
+
+    return null;
+  }
   /// Retrieves all interventions asynchronously.
   ///
   /// Sends a GET request to the API endpoint `${Environment.apiUrl}/interventions`.
