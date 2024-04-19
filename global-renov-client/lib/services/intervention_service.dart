7c057c30fe6478cbf2f2a0931c7d5ba9ce0bd574
242darkman <bvouvou@gmail.com>
2024-04-19 03:27:41 +0200

    feat: add `PATCH` method which allows us to change status intervention

--- global-renov-client/lib/services/intervention_service.dart
+++ global-renov-client/lib/services/intervention_service.dart
@@ -12,6 +12,44 @@ class InterventionService {
   }
 
 
+  /// Asynchronously changes the status of an intervention with the given [status] and [idIntervention].
+  ///
+  /// Sends a PATCH request to the API endpoint `${Environment.apiUrl}/interventions/change-status/$idIntervention`
+  /// with the provided [status]. The request includes the necessary headers, including the authorization token obtained
+  /// from `PreferenceService.getToken()`.
+  ///
+  /// If the request is successful (status code 200), the function returns the decoded response body as a `Map<String, dynamic>`.
+  /// If an error occurs during the request, the function prints the error and returns `null`.
+  ///
+  /// Parameters:
+  /// - `status`: The new status of the intervention.
+  /// - `idIntervention`: The ID of the intervention to be updated.
+  ///
+  /// Returns a `Future<Map<String, dynamic>?>` representing the result of the API call.
+  Future<Map<String, dynamic>?> changeStatusIntervention(
+      String status, String idIntervention) async {
+    try {
+      final response = await http.patch(
+        Uri.parse(
+            '${Environment.apiUrl}/interventions/change-status/$idIntervention'),
+        body: jsonEncode({'status': status}),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $authToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+    } catch (e) {
+      log.severe('Error changing status intervention: $e');
+      return null;
+    }
+
+    return null;
+  }
+
   /// Asynchronously deletes an intervention with the given [idIntervention].
   ///
   /// Sends a DELETE request to the API endpoint `${Environment.apiUrl}/interventions/delete/$idIntervention`
