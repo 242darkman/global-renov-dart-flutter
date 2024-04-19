46caad9a586f2a0e7e20a0a5106484b3dd70d1e7
242darkman <bvouvou@gmail.com>
2024-04-19 03:16:52 +0200

    fix: improve perf

--- global-renov-client/lib/main.dart
+++ global-renov-client/lib/main.dart
@@ -8,7 +8,7 @@ import 'state/app_state.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PreferenceService.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         title: 'Global\'Renov',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(),
-        home: LoginScreen(),
+        home: const LoginScreen(),
       ),
     );
   }
