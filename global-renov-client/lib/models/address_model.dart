f0db7e6f9326b24c0e9cd5c5f69a912dabcb69db
242darkman <bvouvou@gmail.com>
2024-04-19 03:18:30 +0200

    fix: change int id to string id and add model conversion to json

--- global-renov-client/lib/models/address_model.dart
+++ global-renov-client/lib/models/address_model.dart
@@ -5,6 +5,14 @@ class Address {
 
   Address({required this.street, required this.city, required this.postalCode});
 
+  Map<String, dynamic> toJson() {
+    return {
+      'street': street,
+      'city': city,
+      'postalCode': postalCode,
+    };
+  }
+
   factory Address.fromJson(Map<String, dynamic> json) {
     return Address(
       street: json['street'] as String,
